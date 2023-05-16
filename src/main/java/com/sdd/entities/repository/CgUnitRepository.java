a19cc63484505141b87ce41ee6e834d760e888e2
DiwakarSinghRajput <diwakarrocks92@gmail.com>
2023-05-16 12:34:05 +0530

    Changes for dashboard

--- src/main/java/com/sdd/entities/repository/CgUnitRepository.java
+++ src/main/java/com/sdd/entities/repository/CgUnitRepository.java
@@ -1,6 +1,5 @@
 package com.sdd.entities.repository;
 
-
 import com.sdd.entities.CgUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,21 +8,20 @@ import java.util.List;
 
 public interface CgUnitRepository extends JpaRepository<CgUnit, Long> {
 
+  CgUnit findByUnit(String cbUnit);
 
-    CgUnit findByUnit(String cbUnit);
+  @Query(value = "SELECT DESCR FROM cgunit where UNIT=:unitId", nativeQuery = true)
+  String findUnitName(String unitId);
 
-    @Query(value = "SELECT DESCR FROM cgunit where UNIT=:unitId", nativeQuery = true)
-    String findUnitName(String unitId);
+  List<CgUnit> findAllByOrderByDescrAsc();
 
-    List<CgUnit> findAllByOrderByDescrAsc();
+  List<CgUnit> findByPurposeCodeOrPurposeCodeOrderByDescrAsc(String purpose, String purpose1);
 
-    List<CgUnit> findByPurposeCodeOrPurposeCodeOrderByDescrAsc(String purpose, String purpose1);
+  List<CgUnit> findBySubUnitOrderByDescrAsc(String subUnit);
 
-    List<CgUnit> findBySubUnitOrderByDescrAsc(String subUnit);
+  List<CgUnit> findByPurposeCodeOrderByDescrAsc(String purpose);
 
-    List<CgUnit> findByPurposeCodeOrderByDescrAsc(String purpose);
+  List<CgUnit> findByBudGroupUnitLike(String unitId);
 
-    List<CgUnit> findByBudGroupUnitLike(String unitId);
-
-//    CgUnit findByUnit(String authorityUnit);
+  //    CgUnit findByUnit(String authorityUnit);
 }
