fc19d3c867ef6a914ba4358070e3d6a783293f18
Arvind <arvindrajcs@gmail.com>
2023-05-29 18:33:52 +0100

    rebase. reports.. 30-05-23

--- src/main/java/com/sdd/service/Impl/MangeReportImpl.java
+++ src/main/java/com/sdd/service/Impl/MangeReportImpl.java
@@ -27,6 +27,7 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import java.io.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -3542,7 +3543,7 @@ public class MangeReportImpl implements MangeReportService {
         HrData hrData = hrDataRepository.findByUserNameAndIsActive(currentLoggedInUser.getPreferred_username(), "1");
         List<FilePathResponse> dtoList = new ArrayList<FilePathResponse>();
 
-        String amountTypeId = "101";
+        String amountTypeId="101";
 
         if (hrData == null) {
             throw new SDDException(HttpStatus.UNAUTHORIZED.value(), "INVALID TOKEN.LOGIN AGAIN");
@@ -3587,8 +3588,8 @@ public class MangeReportImpl implements MangeReportService {
         LocalDateTime localDateTime = LocalDateTime.of(resultDt, LocalTime.MIDNIGHT);
         Timestamp toDateFormate = Timestamp.valueOf(localDateTime);
 
-        List<String> groupUnitId = budgetRebaseRepository.findGroupRebaseUnit();
-        if (groupUnitId.size() <= 0) {
+        List<String> groupUnitId=budgetRebaseRepository.findGroupRebaseUnit();
+        if(groupUnitId.size()<=0){
             return ResponseUtils.createFailureResponse(dtoList, new TypeReference<List<FilePathResponse>>() {
             }, "DATA NOT FOUND FROM DB", HttpStatus.OK.value());
         }
@@ -3597,7 +3598,7 @@ public class MangeReportImpl implements MangeReportService {
         try {
             //htmlContent = FileUtils.readFileToString(new File("src/main/resources/templates/be-allocation-report.html"), "UTF-8");
             //htmlContent = FileUtils.readFileToString(new File(new File(".").getCanonicalPath()+"/webapps/budget/WEB-INF/classes/templates/be-allocation-report"), "UTF-8");
-            htmlContent = "<!DOCTYPE html>\n" +
+            htmlContent ="<!DOCTYPE html>\n" +
                     "<html lang=\"en\">\n" +
                     "<head>\n" +
                     "    <title>Report</title>\n" +
@@ -3864,46 +3865,46 @@ public class MangeReportImpl implements MangeReportService {
                     "</body>\n" +
                     "</html>\n";
             StringBuilder sb = new StringBuilder();
-            StringBuilder sb1 = new StringBuilder();
+            StringBuilder sb1=new StringBuilder();
 
             int i = 1;
             String finyear = "";
             String unit = "";
 
-            String RunitId = "";
-            String uName = "";
-            String frmStation = "";
-            String toStation = "";
-            String finYear = "";
-            String subHead = "";
-            String headCodeId = "";
+            String RunitId="";
+            String uName="";
+            String frmStation="";
+            String toStation="";
+            String finYear="";
+            String subHead="";
+            String headCodeId="";
             Double allocAmount;
             Double expAmount;
             Double balAmount;
-            Date LastCbD;
-            String val = "";
+            Timestamp LastCbD;
+            String val="";
             Double amountUnit;
             AllocationType allocType;
-            if (groupUnitId.size() > 0) {
-                int count = 1;
+            if(groupUnitId.size()>0) {
+                int count=1;
                 for (String ids : groupUnitId) {
 
-                    RunitId = ids;
-                    List<BudgetRebase> rebaseDatas = budgetRebaseRepository.findByRebaseUnitId(RunitId);
+                    RunitId=ids;
+                    List<BudgetRebase> rebaseDatas=budgetRebaseRepository.findByRebaseUnitId(RunitId);
                     List<BudgetRebase> rebaseData = rebaseDatas.stream()
                             .filter(e -> e.getOccuranceDate().after(fromDateFormate) && e.getOccuranceDate().before(toDateFormate)).collect(Collectors.toList());
-                    if (rebaseData.size() <= 0) {
+                    if(rebaseData.size()<=0){
                         throw new SDDException(HttpStatus.UNAUTHORIZED.value(), "DATA NOT FOUND IN THIS DATE RANGE");
                     }
                     CgUnit unitN = cgUnitRepository.findByUnit(RunitId);
-                    String frmS = rebaseData.get(0).getFrmStationId();
-                    CgStation toS = cgStationRepository.findByStationId(rebaseData.get(0).getToStationId());
+                    String frmS=rebaseData.get(0).getFrmStationId();
+                    CgStation toS= cgStationRepository.findByStationId(rebaseData.get(0).getToStationId());
 
-                    uName = unitN.getDescr();
-                    Date rebaseDate = rebaseData.get(0).getOccuranceDate();
+                    uName=unitN.getDescr();
+                    Date rebaseDate=rebaseData.get(0).getOccuranceDate();
                     System.out.println("RBDATE" + rebaseDate);
-                    frmStation = frmS;
-                    toStation = toS.getStationName();
+                    frmStation=frmS;
+                    toStation=toS.getStationName();
                     sb1.append("<table class=\"dcf-table dcf-table-responsive dcf-table-bordered dcf-table-striped dcf-w-100% \">\t\t<tbody>\n" +
                             "\t\t\t<tr>\n" +
                             "\t\t\t\t<th class=\"dcf-txt-left\">Unit Name</th>\n" +
@@ -3932,45 +3933,50 @@ public class MangeReportImpl implements MangeReportService {
                             "\t\t\t<th class=\"dcf-txt-left\" scope=\"col\">Last CB Date</th>\n" +
                             "\t\t</tr>\n" +
                             "\t</thead>\n" +
-                            "\t<tbody>\n" +
+                            "\t<tbody>\n"+
                             "\t\t\t${data_placeholder}\n" +
                             "\t</tbody>\n" +
                             "</table>\n");
-                    val = val + "\n" + "Serial No: " + (count++) + "\n" + sb1.toString();
-                    sb1 = new StringBuilder();
+                    val=val+"\n"+"Serial No: "+(count++)+"\n"+sb1.toString();
+                    sb1=new StringBuilder();
                     for (Integer k = 0; k < rebaseData.size(); k++) {
                         BudgetFinancialYear findyr = budgetFinancialYearRepository.findBySerialNo(rebaseData.get(k).getFinYear());
                         BudgetHead bHead = subHeadRepository.findByBudgetCodeId(rebaseData.get(k).getBudgetHeadId());
                         AmountUnit amountTypeObj = amountUnitRepository.findByAmountTypeId(rebaseData.get(k).getAmountType());
-                        allocType = allocationRepository.findByAllocTypeId(rebaseData.get(k).getAllocTypeId());
+                        allocType=allocationRepository.findByAllocTypeId(rebaseData.get(k).getAllocTypeId());
                         if (amountTypeObj == null) {
                             return ResponseUtils.createFailureResponse(dtoList, new TypeReference<List<FilePathResponse>>() {
                             }, "AMOUNT TYPE NOT FOUND FROM DB", HttpStatus.OK.value());
                         }
                         amountUnit = amountTypeObj.getAmount();
-                        finYear = findyr.getFinYear();
-                        subHead = bHead.getSubHeadDescr();
-                        headCodeId = bHead.getBudgetHeadId();
-                        Double aAmount = Double.valueOf(rebaseData.get(k).getAllocAmount());
-                        Double eAmount = Double.valueOf(rebaseData.get(k).getExpAmount());
-                        Double bAmount = Double.valueOf(rebaseData.get(k).getBalAmount());
-                        LastCbD = rebaseData.get(k).getLastCbDate();
+                        finYear=findyr.getFinYear();
+                        subHead=bHead.getSubHeadDescr();
+                        headCodeId=bHead.getBudgetHeadId();
+                        Double aAmount=Double.valueOf(rebaseData.get(k).getAllocAmount());
+                        Double eAmount=Double.valueOf(rebaseData.get(k).getExpAmount());
+                        Double bAmount=Double.valueOf(rebaseData.get(k).getBalAmount());
+                        LastCbD=rebaseData.get(k).getLastCbDate();
+                        SimpleDateFormat id = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+                        SimpleDateFormat od = new SimpleDateFormat("dd-MMMM-yyyy");
+                        Date dateC = id.parse(LastCbD.toString());
+                        String cbD = od.format(dateC);
+
                         allocAmount = aAmount * amountUnit / reqAmount;
-                        expAmount = eAmount * amountUnit;
+                        expAmount = eAmount;
                         balAmount = bAmount * amountUnit / reqAmount;
 
                         sb.append("<tr>");
-                        sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(finYear + " " + allocType.getAllocDesc())).append("</td>");
+                        sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(finYear+" "+allocType.getAllocDesc())).append("</td>");
                         sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(subHead)).append("</td>");
                         //sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(headCodeId)).append("</td>");
                         sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(String.format("%1$0,1.4f", new BigDecimal(allocAmount)))).append("</td>");
                         sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(String.format("%1$0,1.4f", new BigDecimal(expAmount)))).append("</td>");
                         sb.append("<td class=\"dcf-txt-left\">").append(StringEscapeUtils.escapeHtml4(String.format("%1$0,1.4f", new BigDecimal(balAmount)))).append("</td>");
-                        sb.append("<td class=\"dcf-txt-left\">").append(LastCbD).append("</td>");
+                        sb.append("<td class=\"dcf-txt-left\">").append(cbD).append("</td>");
                         sb.append("</tr>");
                     }
-                    val = val.replace("${data_placeholder}", sb.toString());
-                    sb = new StringBuilder();
+                    val=val.replace("${data_placeholder}",sb.toString());
+                    sb=new StringBuilder();
                 }
             }
 
@@ -3982,12 +3988,12 @@ public class MangeReportImpl implements MangeReportService {
             htmlContent = htmlContent.replace("${upToDate_placeholder}", StringEscapeUtils.escapeHtml4(formattedDate));
             htmlContent = htmlContent.replace("${finYear_placeholder}", StringEscapeUtils.escapeHtml4(finYear));
             htmlContent = htmlContent.replace("${amountType_placeholder}", StringEscapeUtils.escapeHtml4(amountIn));
-            String filepath = HelperUtils.FILEPATH + "/" + "Rebase_Report.pdf";
+            String filepath = HelperUtils.FILEPATH +"/"+"Rebase_Report.pdf";
             File folder = new File(new File(".").getCanonicalPath() + HelperUtils.LASTFOLDERPATH);
             if (!folder.exists()) {
                 folder.mkdirs();
             }
-            String filePath = folder.getAbsolutePath() + "/" + "Rebase_Report.pdf";
+            String filePath = folder.getAbsolutePath()+"/"+"Rebase_Report.pdf";
             File file = new File(filePath);
             generatePdf(htmlContent, file.getAbsolutePath());
             //generatePdf(htmlContent, filepath);
@@ -4003,6 +4009,7 @@ public class MangeReportImpl implements MangeReportService {
         return ResponseUtils.createSuccessResponse(dtoList, new TypeReference<List<FilePathResponse>>() {
         });
     }
+
 
 
     @Override
