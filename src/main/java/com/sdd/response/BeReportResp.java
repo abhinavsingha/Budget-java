b29a7699fc0828553e2c1ed6f477cc7f7edc4bd7
Arvind <arvindrajcs@gmail.com>
2023-06-13 11:50:49 +0530

    missing file

---
+++ src/main/java/com/sdd/response/BeReportResp.java
@@ -0,0 +1,17 @@
+package com.sdd.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BeReportResp {
+    String allocationType;
+    String finYear;
+    String amountIn;
+
+    String budgetHead;
+    String unitName;
+    String allocationAmount;
+
+}
