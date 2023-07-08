6ca148602426d17124924bb08ffde54dd66f1039
apple <Happy_112233>
2023-07-08 13:29:41 +0530

    Working round Recipt Report

--- src/main/java/com/sdd/service/Impl/BudgetAllocationServiceImpl.java
+++ src/main/java/com/sdd/service/Impl/BudgetAllocationServiceImpl.java
@@ -1469,7 +1469,7 @@ public class BudgetAllocationServiceImpl implements BudgetAllocationService {
 
                     List<CdaParkingCrAndDr> cdaParkingCrAndDr = parkingCrAndDrRepository.findByFinYearIdAndBudgetHeadIdAndGinNoAndIsFlagAndAndAllocTypeIdAndUnitIdAndIsRevision(cdaParkingTrans.getFinYearId(), cdaParkingTrans.getBudgetHeadId(), cdaParkingTrans.getGinNo(), "0", cdaParkingTrans.getAllocTypeId(), budgetAllocationSaveRequestList.getBudgetRequest().get(i).getToUnitId(), 0);
                     for (Integer q = 0; q < cdaParkingTransList.size(); q++) {
-                        CdaParkingCrAndDr cddata = cdaParkingCrAndDr.get(i);
+                        CdaParkingCrAndDr cddata = cdaParkingCrAndDr.get(q);
                         cddata.setIsFlag("1");
                         parkingCrAndDrRepository.save(cddata);
                     }
