6088a2a1e2cf1f566328e07752558ce3ef94103e
Arvind <arvindrajcs@gmail.com>
2023-07-30 22:16:57 +0530

    Rebase  30-07-23

--- src/main/java/com/sdd/service/Impl/MangeRebaseImpl.java
+++ src/main/java/com/sdd/service/Impl/MangeRebaseImpl.java
@@ -328,7 +328,7 @@ public class MangeRebaseImpl implements MangeRebaseService {
         }
         List<BudgetAllocation> allocationData1 = budgetAllocationRepository.findByToUnitAndFinYearAndAllocationTypeIdAndIsBudgetRevision(unit, finYear, allocTypes, "0");
         List<BudgetAllocation> allocationData2 = allocationData1.stream().filter(e -> e.getStatus().equalsIgnoreCase("Approved") && e.getIsFlag().equalsIgnoreCase("0")).collect(Collectors.toList());
-        List<BudgetAllocation>allocationData=allocationData2.stream().filter(e->!e.getAllocationAmount().equalsIgnoreCase("0")).collect(Collectors.toList());;
+        List<BudgetAllocation>allocationData=allocationData2.stream().filter(e->!e.getAllocationAmount().equalsIgnoreCase(String.valueOf(0.0000))).collect(Collectors.toList());;
         if (allocationData.size() <= 0) {
             return ResponseUtils.createFailureResponse(responce, new TypeReference<List<RebaseBudgetHistory>>() {
             }, "Record Not Found", HttpStatus.OK.value());
