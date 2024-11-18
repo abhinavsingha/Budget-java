0b600c74cd740caf70851918c22025972848266e
Ishaan Kumar <i.k.ishaan786@gmail.com>
2024-11-18 15:51:25 +0530

    CERA point for keycloak for authonticate token

--- src/main/java/com/sdd/jwt/HeaderUtils.java
+++ src/main/java/com/sdd/jwt/HeaderUtils.java
@@ -3,19 +3,22 @@ package com.sdd.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.sdd.entities.AllocationType;
 import com.sdd.exception.SDDException;
 import com.sdd.jwtParse.TokenParseData;
+import com.sdd.response.DefaultResponse;
+import com.sdd.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -24,7 +27,7 @@ import java.util.Map;
 
 @Component
 public class HeaderUtils {
-
+  DefaultResponse defaultResponse = new DefaultResponse();
   public static final String USER_ID = "user_id";
   public static final String EMAIL_ID = "user_id";
   public static final String MOBILE_NO = "mobile_no";
@@ -36,6 +39,7 @@ public class HeaderUtils {
   {
 
     String tokenWithoutBearer = "";
+
     String token = httpServletRequest.getHeader("Authorization");
     if (token == null) {
       throw new SDDException(HttpStatus.UNAUTHORIZED.value(), "INVALID TOKEN. LOGIN AGAIN.IN-001");
@@ -51,15 +55,25 @@ public class HeaderUtils {
     }
     https://icg.net.in/auth/realms/icgrms/protocol/openid-connect/token/introspect
 
+    try {
+      validateToken(token);
+      if (validateToken(token).equals("true")) {
+        //System.out.println("Active: ");
+        //throw new SDDException(HttpStatus.UNAUTHORIZED.value(), "Token not expired");
+        //throw new SDDException(HttpStatus.UNAUTHORIZED.value(), "INVALID TOKEN. LOGIN AGAIN.IN-004");
+      } else {
+      //  System.out.println("DeActive: ");
 
-   // validateToken(tokenWithoutBearer);
+        throw new SDDException(HttpStatus.UNAUTHORIZED.value(), "INVALID TOKEN. LOGIN AGAIN.IN-005");
+      }
+    }catch (Exception ed){
 
-
-
-
-
+    }
     return tokenWithoutBearer;
   }
+
+
+
 //  public String getTokeFromHeader() {
 //
 //    String tokenWithoutBearer = "";
@@ -125,46 +139,62 @@ public class HeaderUtils {
 
     return exampleData;
   }
-  public String validateToken(String tokenWithoutBearer) throws IOException {
-    String url = "https://icg.net.in/auth/realms/icgrms/protocol/openid-connect/token/introspect";
-    String token = tokenWithoutBearer;
-    String Authorization = "BASIC Auth";
-    String client_id = "budget";
-    String client_secret = "cKTyGSHisNKdDY7qEjKIA2eHZT3DtN3i";
+  public static String validateToken(String tokenWithoutBearer) {
+    StringBuilder response = new StringBuilder();
+    try {
+      String url = "https://icg.net.in/auth/realms/icgrms/protocol/openid-connect/token/introspect";
+      String token = tokenWithoutBearer;
+      String Authorization = "BASIC Auth";
+      String client_id = "budgetAPI";
+      String client_secret = "2uz6nswgHfwJpiQzHuqnpnjP5fAQGKtf";
 
-    URL obj = new URL(url);
-    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+      URL obj = new URL(url);
+      HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-    // Set the request method to POST
-    con.setRequestMethod("POST");
+      // Set the request method to POST
+      con.setRequestMethod("POST");
 
-    // Set the content type to x-www-form-urlencoded
-    con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-    // Enable output
-    con.setDoOutput(true);
-    // Build the request body
-    String urlParameters = "token=" + URLEncoder.encode(token, "UTF-8")+
-            "&Authorization=" + URLEncoder.encode(Authorization, "UTF-8")+
-            "&client_id=" + URLEncoder.encode(client_id, "UTF-8")+
-            "&client_secret=" + URLEncoder.encode(client_secret, "UTF-8");
-    // Send the request body
-    try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
-      wr.writeBytes(urlParameters);
-      wr.flush();
-    }
-    // Get the response code
-    int responseCode = con.getResponseCode();
-    System.out.println("Response Code : " + responseCode);
-    // Read the response
-    try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
-      String inputLine;
-      StringBuilder response = new StringBuilder();
-      while ((inputLine = in.readLine()) != null) {
-        response.append(inputLine);
+      // Set the content type to x-www-form-urlencoded
+      con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+      // Enable output
+      con.setDoOutput(true);
+      // Build the request body
+
+      String urlParameters = "token=" + URLEncoder.encode(token, "UTF-8") +
+              "&Authorization=" + URLEncoder.encode(Authorization, "UTF-8") +
+              "&client_id=" + URLEncoder.encode(client_id, "UTF-8") +
+              "&client_secret=" + URLEncoder.encode(client_secret, "UTF-8");
+
+
+      // Send the request body
+      try (DataOutputStream wr = new DataOutputStream(con.getOutputStream())) {
+        wr.writeBytes(urlParameters);
+        wr.flush();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
       }
-      System.out.println("Response : " + response.toString());
-      return   response.toString();
+      // Get the response code
+      int responseCode = con.getResponseCode();
+      System.out.println("Response Code : " + responseCode);
+      // Read the response
+      try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+        String inputLine;
+
+        while ((inputLine = in.readLine()) != null) {
+          response.append(inputLine);
+        }
+        System.out.println("Response : " + response.toString());
+      }
+      ObjectMapper mapper = new ObjectMapper();
+      JsonNode rootNode = mapper.readTree(String.valueOf(response));
+      boolean isActive = rootNode.path("active").asBoolean();
+      //System.out.println("Active: " + isActive);
+      return response.toString();
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
-
 }
+
